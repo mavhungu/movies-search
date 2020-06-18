ae50e38c38c5d246b073f10c66188645a1c5f973
ronewa <ronewamavhungu@gmail.com>
2020-06-18 22:08:07 +0200

    change default movie

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,16 @@ var fetch = require('node-fetch');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  fetch(`http://www.omdbapi.com/?apikey=ebb5d535&s=man&page=2`)
+  fetch(`http://www.omdbapi.com/?apikey=ebb5d535&s=man&type=movie`)
       .then(res=> res.json())
       .then(body => { const tt = body;
         res.render('index', {
           name: {tt}
         });
-        //console.log(tt)
       })
       .catch((e)=>{
         res.status(500).send(e);
       });
-  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
