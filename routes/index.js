20513d457fadd16a1677d3138cfad37dcf5804ac
ronewa <ronewamavhungu@gmail.com>
2020-06-18 21:01:36 +0200

    adding deflault movies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  fetch(`http://www.omdbapi.com/?apikey=ebb5d535&s=man&page=2`)
+      .then(res=> res.json())
+      .then(body => { const tt = body;
+        res.render('index', {
+          name: {tt}
+        });
+        //console.log(tt)
+      })
+      .catch((e)=>{
+        res.status(500).send(e);
+      });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
